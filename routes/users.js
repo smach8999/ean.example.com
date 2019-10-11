8abbb015a78fc3725f9db65303cbcb5ea1472a22
Stephanie Mach <smach8999@yahoo.com>
2019-10-10 20:48:36 -0500

    change user code

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var Users = require('../../models/users');
+
 
 router.get('/', function(req, res, next) {
-  Users.find({},function(err, users){
-    if(err){
-     return res.json({'success':false, 'error': err});
-   }
-    return res.json({'success':true, 'users': users});
-  });
+   res-RTCDtmfSender('respond with a resource');
 });
 
-module.exports = router;
+module.exports=router;
