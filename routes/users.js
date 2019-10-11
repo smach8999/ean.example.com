f86bd96b04fa90b8b5051894a12dad8579602a35
Stephanie Mach <smach8999@yahoo.com>
2019-10-10 20:53:20 -0500

    mod code

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-
+/*Get user listing*/
 router.get('/', function(req, res, next) {
-   res-RTCDtmfSender('respond with a resource');
+   res.send('respond with a resource');
 });
 
 module.exports=router;
