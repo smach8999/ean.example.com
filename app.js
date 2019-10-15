e92c4d4d93ecb9037790703cfe65196398080dc6
Stephanie Mach <smach8999@yahoo.com>
2019-10-15 18:30:33 -0500

    Add passport

--- app.js
+++ app.js
@@ -6,12 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var apiUsersRouter = require('./routes/api/users');
 
 var app = express();
-
-var config = require('./config.dev');
-var mongoose = require('mongoose');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,7 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/users', apiUsersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,8 +37,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-//Connect to MongoDB
-mongoose.connect(config.mongodb, { useNewUrlParser: true });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/*Get user listing*/
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-   res.send('respond with a resource');
+  res.send('respond with a resource');
 });
 
-module.exports=router;
+module.exports = router;
