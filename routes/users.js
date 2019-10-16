63942324f67d48cf8be1fd30389f98b6554ea04c
Stephanie Mach <smach8999@yahoo.com>
2019-10-15 19:41:43 -0500

    Modified

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var Users = require('../models/users');
 
-/* GET users listing. */
+// router.get('/', function(req, res) {
+//   res.send('respond with a resource');
+// });
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  Users.find({},function(err, users){
+    if(err){
+     return res.json({'success':false, 'error': err});
+    }
+
+    return res.json({'success':true, 'users': users});
+  });
+
 });
 
 module.exports = router;
