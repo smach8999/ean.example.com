d4577218c26a0517d6b2f3cc5ca7e595a77a6c98
Stephanie Mach <smach8999@yahoo.com>
2019-10-16 17:32:05 -0500

    Setup ExpressJS with Pug

--- app.js
+++ app.js
@@ -6,19 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var apiUsersRouter = require('./routes/api/users');
-var LocalStrategy = require('passport-local').Strategy;
-var Users = require('./models/users');
-var apiAuthRouter = require('./routes/api/auth');
-var authRouter = require('./routes/auth');
 
 var app = express();
-
-var config = require('./config.dev');
-var mongoose = require('mongoose');
-var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
-var passport = require('passport');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,49 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('express-session')({
-  //Define the session store
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection
-  }),
-  //Set the secret
-  secret: config.session.secret,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    path: '/',
-    domain: config.cookie.domain,
-    //httpOnly: true,
-    //secure: true,
-    maxAge:3600000 //1 hour
-  }
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/users', apiUsersRouter);
-
-passport.use(Users.createStrategy());
-
-//~line 53
-passport.serializeUser(function(user, done){
-  done(null,{
-    id: user._id,
-    username: user.username,
-    email: user.email,
-    first_name: user.first_name,
-    last_name: user.last_name
-  });
-});
-
-passport.deserializeUser(function(user, done){
-  done(null, user);
-});
-
-app.use('/api/auth', apiAuthRouter);
-app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -89,8 +37,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-//Connect to MongoDB
-mongoose.connect(config.mongodb, { useNewUrlParser: true });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var Users = require('../models/users');
 
-// router.get('/', function(req, res) {
-//   res.send('respond with a resource');
-// });
-
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-
-  Users.find({},function(err, users){
-    if(err){
-     return res.json({'success':false, 'error': err});
-    }
-
-    return res.json({'success':true, 'users': users});
-  });
-
+  res.send('respond with a resource');
 });
 
 module.exports = router;
