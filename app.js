d23ef5857357fae97a0a1284c7b401ec94068c97
Stephanie Mach <smach8999@yahoo.com>
2019-10-15 20:57:23 -0500

    Mod Express

--- app.js
+++ app.js
@@ -4,21 +4,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var session = require('express-session');
-var mongoose = require('mongoose');
-var passport = require('passport');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiUsersRouter = require('./routes/api/users');
 var LocalStrategy = require('passport-local').Strategy;
-var app = express();
 var Users = require('./models/users');
-var config = require('./config.dev');
 var apiAuthRouter = require('./routes/api/auth');
+
+var app = express();
+
+var config = require('./config.dev');
+var mongoose = require('mongoose');
+var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
-//Connect to MongoDB
-mongoose.connect(config.mongodb, { useNewUrlParser: true });
+var passport = require('passport');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,10 +28,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/api/users', apiUsersRouter);
 
 app.use(require('express-session')({
   //Define the session store
@@ -54,8 +49,30 @@ app.use(require('express-session')({
 app.use(passport.initialize());
 app.use(passport.session());
 
-// catch 404 and forward to error handler
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api/users', apiUsersRouter);
+
 passport.use(Users.createStrategy());
+
+//~line 53
+passport.serializeUser(function(user, done){
+  done(null,{
+    id: user._id,
+    username: user.username,
+    email: user.email,
+    first_name: user.first_name,
+    last_name: user.last_name
+  });
+});
+
+passport.deserializeUser(function(user, done){
+  done(null, user);
+});
+
+app.use('/api/auth', apiAuthRouter);
+
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
@@ -73,7 +90,5 @@ app.use(function(err, req, res, next) {
 
 //Connect to MongoDB
 mongoose.connect(config.mongodb, { useNewUrlParser: true });
-
-app.use('/api/auth', apiAuthRouter);
 
 module.exports = app;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var Users = require('../models/users');
-
-// router.get('/', function(req, res) {
-//   res.send('respond with a resource');
-// });
+var Users = require('../../models/users');
 
 router.get('/', function(req, res, next) {
 
@@ -17,5 +13,94 @@ router.get('/', function(req, res, next) {
   });
 
 });
+
+router.get('/:userId', function(req,res){
+  
+  var userId = req.params.userId;
+
+  Users.findOne({'_id':userId}, function(err, user){
+    if(err){
+      return res.json({'success':false, 'error': err});
+    }
+
+    return res.json({'success':true, 'user': user});
+  });
+
+});
+
+router.post('/', function(req, res) {
+  Users.create(new Users({
+    username: req.body.username,
+    email: req.body.email,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name
+  }), function(err, user){
+    
+    if(err){
+      return res.json({success: false, user: req.body, error: err});
+    }
+
+    return res.json({success: true, user: user});
+    
+  });
+});
+
+router.put('/', function(req, res){
+
+    Users.findOne({'_id': req.body._id}, function(err, user){
+  
+     if(err) {
+       return res.json({success: false, error: err});
+     }
+  
+     if(user) {
+  
+      let data = req.body;
+  
+      if(data.username){
+        user.username = data.username;
+      };
+  
+      if(data.email){
+      user.email = data.email;
+      };
+  
+      if(data.first_name){
+      user.first_name = data.first_name;
+      };
+  
+      if(data.last_name){
+      user.last_name = data.last_name;
+      };
+  
+      user.save(function(err){
+        if(err){
+          return res.json({success: false, error: err});
+        }else{
+          return res.json({success: true, user:user});
+        }
+      });
+  
+     }
+  
+    });
+    
+  });
+  router.delete('/:userId', function(req,res){
+
+    var userId = req.params.userId;
+  
+    Users.remove({'_id':userId}, function(err,removed){
+  
+      if(err){
+        return res.json({success: false, error: err});
+      }
+  
+      return res.json({success: true, status: removed});
+  
+    });
+  
+  });
+    
 
 module.exports = router;
