889964fd1b3896970eca08137575660e6f0575d7
Stephanie Mach <smach8999@yahoo.com>
2019-10-15 21:40:48 -0500

    Setup ExpressJS with Pug

---
+++ routes/auth.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('auth/index', { title: 'User Authentication' });
+});
+module.exports = router;
