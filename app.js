7f697adb7996b81b9487d73aa6b61d6e36338f1a
Stephanie Mach <smach8999@yahoo.com>
2019-10-17 00:48:46 -0500

    Define a local strategy for authentication

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ var mongoose = require('mongoose');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 var passport = require('passport');
-
+var LocalStrategy = require('passport-local').Strategy;
 var indexRouter = require('./routes/index');
+var Users = require('./models/users');
 var usersRouter = require('./routes/users');
 var apiUsersRouter = require('./routes/api/users');
 
@@ -49,6 +50,7 @@ app.use(require('express-session')({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(Users.createStrategy());
 passport.serializeUser(function(user, done){
   done(null,{
     id: user._id,
