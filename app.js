b0531e79308816bbcef949cd24ec5a4c128eb298
Stephanie Mach <smach8999@yahoo.com>
2019-10-16 23:52:35 -0500

    Add api/users route to apps

--- app.js
+++ app.js
@@ -1,8 +1,51 @@
-var config = require('./config.dev');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 var mongoose = require('mongoose');
 
-//Test the file
-// console.log(config);
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var apiUsersRouter = require('./routes/api/users');
+
+var app = express();
+
+//Call the config file
+var config = require('./config.dev');
 
 //Connect to MongoDB
 mongoose.connect(config.mongodb, { useNewUrlParser: true });
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api/users', apiUsersRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
