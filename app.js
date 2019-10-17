8c7cab3ea75969ca47b9ef77a9e4112ef90f08fa
Stephanie Mach <smach8999@yahoo.com>
2019-10-16 23:32:50 -0500

    Connect to the database

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var config = require('./config.dev');
+var mongoose = require('mongoose');
 
 //Test the file
 // console.log(config);
+
+//Connect to MongoDB
+mongoose.connect(config.mongodb, { useNewUrlParser: true });
