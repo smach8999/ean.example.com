d36a46df72bd295afc6d6a87bc683ca38e484d69
Stephanie Mach <smach8999@yahoo.com>
2019-10-17 19:15:13 -0500

    Add an XHR post request method

--- src/js/auth.app.js
+++ src/js/auth.app.js
@@ -1,9 +1,9 @@
-var authApp = (function() {
+var authApp = (function () {
 
-    function loginForm(){
-      let app = document.getElementById('app');
-  
-      let form =  `
+  function loginForm() {
+    let app = document.getElementById('app');
+
+    let form = `
         <div class="card login-form">
           <form id="loginForm" class="card-body">
             <h1 class="card-title text-center">Please Sign In</h1>
@@ -22,18 +22,45 @@ var authApp = (function() {
           </form>
         </div>
       `;
-  
-      app.innerHTML=form;
-    }
 
-    
-  
-    return {
-      load: function(){
-        loginForm();
+    app.innerHTML = form;
+  }
+
+  function postRequest(formId, url) {
+    let form = document.getElementById(formId);
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+
+      let formData = new FormData(form);
+      let uri = `${window.location.origin}${url}`;
+      let xhr = new XMLHttpRequest();
+      xhr.open('POST', uri);
+
+      xhr.setRequestHeader(
+        'Content-Type',
+        'application/json; charset=UTF-8'
+      );
+
+      let object = {};
+      formData.forEach(function (value, key) {
+        object[key] = value;
+      });
+
+      xhr.send(JSON.stringify(object));
+      xhr.onload = function () {
+        let data = JSON.parse(xhr.response);
+        console.log(data);
       }
+    });
+  }
+
+  return {
+    load: function () {
+      loginForm();
+      postRequest('loginForm', '/api/auth/login');
     }
-  
-  })();
-  
-  authApp.load();
+  }
+
+})();
+
+authApp.load();
