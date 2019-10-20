5c31a1b263411e9032ceb3154c0f7c37c3d20662
Stephanie Mach <smach8999@yahoo.com>
2019-10-20 07:33:38 -0500

    Edit a user record

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Users = require('../../models/users');
 
+// Get all users
 // router.get('/', function(req, res, next) {
 //   Users.find({},function(err, users){
 //     if(err){
@@ -22,4 +23,63 @@ router.get('/:userId', function(req,res){
    });
  });
 
+//  POST request to the api/users endpoint shall create a new user record
+ router.post('/', function(req, res) {
+  Users.create(new Users({
+    username: req.body.username,
+    email: req.body.email,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name
+  }), function(err, user){
+    
+    if(err){
+      return res.json({success: false, user: req.body, error: err});
+    }
+
+    return res.json({success: true, user: user});
+    
+  });
+});
+
+router.put('/', function(req, res){
+
+  Users.findOne({'_id': req.body._id}, function(err, user){
+
+   if(err) {
+     return res.json({success: false, error: err});
+   }
+
+   if(user) {
+
+    let data = req.body;
+
+    if(data.username){
+      user.username = data.username;
+    };
+
+    if(data.email){
+    user.email = data.email;
+    };
+
+    if(data.first_name){
+    user.first_name = data.first_name;
+    };
+
+    if(data.last_name){
+    user.last_name = data.last_name;
+    };
+
+    user.save(function(err){
+      if(err){
+        return res.json({success: false, error: err});
+      }else{
+        return res.json({success: true, user:user});
+      }
+    });
+
+   }
+
+  });
+  
+});
 module.exports = router;
