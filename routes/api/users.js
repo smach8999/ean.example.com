e77113db2dc0cb1545c00496df5f11da2943f6ff
Stephanie Mach <smach8999@yahoo.com>
2019-10-22 18:23:38 -0500

    Add gulp processes for the user app

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 var Users = require('../../models/users');
 
-// Get all users
-// router.get('/', function(req, res, next) {
-//   Users.find({},function(err, users){
-//     if(err){
-//      return res.json({'success':false, 'error': err});
-//    }
-//     return res.json({'success':true, 'users': users});
-//   });
-// });
+Get all users
+router.get('/', function(req, res, next) {
+  Users.find({},function(err, users){
+    if(err){
+     return res.json({'success':false, 'error': err});
+   }
+    return res.json({'success':true, 'users': users});
+  });
+});
 
 router.get('/:userId', function(req,res){
   
